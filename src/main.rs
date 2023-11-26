06f99ac9f3c4370472cbcb5f4b83761d62097286
Vikash Roy <chiggos.vikash@gmail.com>
2023-11-26 22:33:10 +0530

    Added shadowning

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,11 @@
-use std::io;
+// use std::io;
 
 fn main() {
-    // println!("Hello, world!");
-    // let tuple_x:(i32,f32,bool) = (12,4.0,false);
-    // println!("type {}",tuple_x.1);
-    // let mut _num = 10;
-    // let name = "Vikash Kumar";
-    // let length = name.len();
-    // println!("Hello this is no: {_num}");
-    // _num = 23;
-    // println!("Length of name {name} is :{length}");
+   
+    // let mut input = String::new();
 
-    // println!("value after assigned :{_num}");
-
-    // take user input
-    // 
-    let mut input = String::new();
-
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    println!("User entered {}",input);
+    // io::stdin().read_line(&mut input).expect("Failed to read line");
+    // println!("User entered {}",input);
 
     let add_result = add_two_number(10, 32);
     println!("Added value is {}",add_result);
@@ -30,6 +17,16 @@ fn main() {
     println!("Multiplication value is {}",mult_result);
     println!("Division value is {}",div_result);
     println!("Subtraction value is {}",sub_result);
+
+    const PI:f32 = 3.14;
+    let area_of_circle_result = area_of_circle(10.0,PI);
+    println!("Area of circle is {}",area_of_circle_result);
+
+    shadowing();
+    }
+
+    fn area_of_circle(radius:f32,pi:f32)-> f32{
+        return pi*radius*radius;
     }
 
     fn add_two_number(a:i32,b:i32)->i32{
@@ -49,5 +46,25 @@ fn main() {
     fn sub_two_number(a:i32,b:i32)->i32{
         return a-b;
     }
+
+
+    /// shadowing
+    /// Shadowing is different from marking a variable as mut, because we’ll get a compile-time 
+    /// error if we accidentally try to reassign to this variable without using the let keyword.
+    ///  By using let, we can perform a few transformations on a value but have the variable be
+    ///  immutable after those transformations have been completed.
+    fn shadowing(){
+       let x:i32 = 10;
+       let x:i32 = x + 12;
+
+       {
+            let x:i32 = x + 12;
+             println!("Value of x is in inner scopre is {}",x);
+       }
+       println!("value of x in outer scope is {}",x);
+
+    }
+
+   
 
     
